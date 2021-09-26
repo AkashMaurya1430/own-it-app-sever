615cf1db9ff089609dff5d1fc793a03db16d7c4d
AkashMaurya1430 <akashmaurya1430@gmail.com>
2021-09-26 11:52:56 +0530

    Hosting

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,10 @@ var express = require("express");
 var router = express.Router();
 const authController = require("../controller/authController");
 
+router.get("/", (req, res) => {
+  res.send("Site Active");
+});
+
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
