33a99294659e09c5a875bbb529b2242ef9ab3e54
AkashMaurya1430 <akashmaurya1430@gmail.com>
2021-10-28 16:27:45 +0530

    hardcode

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -2,9 +2,11 @@ var express = require("express");
 var router = express.Router();
 const appController = require("../controller/appController");
 
-router.get("/advertisements", appController.getAdvertisement);
+router.get("/advertisements", appController.getAdvertisementIDs);
 
 router.post("/advertisement/create", appController.createAdvertisement);
+
+// router.post("/advertisement/", appController.createAdvertisement);
 
 router.post("/advertisement/delete", appController.deleteAdvertisement);
 
